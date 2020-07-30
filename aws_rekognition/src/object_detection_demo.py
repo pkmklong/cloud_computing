c7319333bc3df33ba25dea109dbe034a6c28c9ee
Patrick Long <patrick.long@gmail.com>
2020-07-30 08:45:07 -0400

    update syntax, wrap object_detection_demo.py in main function

--- aws_rekognition/src/object_detection_demo.py
+++ aws_rekognition/src/object_detection_demo.py
@@ -2,16 +2,18 @@ from storage_service import StorageService
 from recognition_service import RecognitionService
 
 
-storage_service = StorageService()
-recogntion_service =  RecognitionService()
+def main(bucket_name):
 
-bucket_name = "aws-bucket-fun"
+    for file in storage_service.get_all_files(bucket_name):
+        if file.key.endswith(".jpg"):
+            print(f"Objects detected in image {file.key} :")
+            labels = recogntion_service.detect_objects(file.bucket_name, file.key)
+            for label in labels:
+                print(f"-- {label['Name']}: {str(label['Confidence'])}")
 
-for file in storage_service.get_all_files(bucket_name):
-    if file.key.endswith(".jpg"):
-        print("Objects detected in image " + file.key + ":")
-        labels = recogntion_service.detect_objects(file.bucket_name, file.key)
-        for label in labels:
-            print("-- "  + label["Name"] + ": " + str(label["Confidence"]))
+if __name__=="__main__":
 
-
+    storage_service = StorageService()
+    recogntion_service =  RecognitionService()
+    bucket_name = "aws-bucket-fun"
+    main(bucket_name)
