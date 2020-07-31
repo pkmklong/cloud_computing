505929787ae2301b507b514eeb8e20f9542b68b2
Patrick Long <patrick.long@gmail.com>
2020-07-30 20:14:32 -0400

    add app.py

--- ObjectDetector/capabilities/app.py
+++ ObjectDetector/capabilities/app.py
@@ -1,12 +1,37 @@
 from chalice import Chalice
+from chalicelib import storage_services
+from chalicelib import recogntion_service
 
-app = Chalice(app_name='capabilities')
+import random
 
+####
+# chalice app configuration
+####
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+app = Chalice(app_name='Capabilities')
+app.debut = True
 
+####
+# service initialization
+####
+storage_location = "aws-bucket-fun"
+storage_service = stoarage_service.StorageService(storage_location)
+recognition_service = recogntion_service.RecognitionService(storage_service)
+
+@app.route('/demo-object-detection', cors = True)
+def demo_object_detection():
+    """randomly slects one image to demo object detection"""
+    files = storage_service.list_files()
+    images = [file for file in files if file["file_name"].endswith(".jpg")]
+    image = random.choice(images)
+
+    objects = recognition_service.detect_objects(images["file_name"])
+
+    return { 
+    "imageName"; image["file_name"],
+    "imageUrl": image["url"],
+    "objects": objects
+    }
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
