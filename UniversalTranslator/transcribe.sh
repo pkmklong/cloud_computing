0e12f8fcf4b351c42e1650c5d3333ee068b46c91
Patrick Long <patrick.long@gmail.com>
2020-08-02 21:19:19 -0400

    update transcribe shell script:

--- UniversalTranslator/transcribe.sh
+++ UniversalTranslator/transcribe.sh
@@ -1,10 +1,10 @@
 function transcribe() {
-aws transcribe start-transcription-job
-    --transcription-job-name testing 
-    --language-code en-US
-    --media-format mp4 
-    --media MediaFileUri=https://s3.amazonaws.com/contents.aws.a/testing/.mp4
-    --output-bucket-name tested 
+aws transcribe start-transcription-job \
+    --transcription-job-name testing \
+    --language-code en-US \
+    --media-format mp4  \
+    --media MediaFileUri=https://s3.amazonaws.com/aws-bucket-fun/testing.mp4 \
+    --output-bucket-name aws-bucket-fun  
 }
 
 transcribe
